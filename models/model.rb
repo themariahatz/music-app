2c72804a28815a3fab88249234ba40ce3b0c97e0
themariahatz <maria.baseballfan@gmail.com>
2015-08-27 19:28:20 +0000

    New

--- models/model.rb
+++ models/model.rb
@@ -1,10 +1,20 @@
-class Result
-  def response(name, emotion)
-    if emotion == "panic" 
-      return "Don't worry!"
-    else
-      return "Me too!"
-    end
-    end 
-end
 
+
+def selection(genre)
+  if genre == "Alternative"
+    return "Joel Michael Howard" + "Mac Demarco" + "Hockey Dad" + "Allah-Las" + "Tame Impala" + "The Front Bottoms"
+
+  elsif genre == "Rock/Garage Rock"
+    return "Thee oh Sees" + "Skegss" + "Ty Segall" + "The Growlers" + "The Coachwhips"
+
+  elsif genre == "Electronic/New Wave"
+    return "Todd Terje" + "Panda bear" + "Chet Faker" + "Motor City Drum Ensemble" + "Flume" + "XXYYXX"
+
+  elsif genre == "Rap/Hip Hop"
+    return "Yung Lean" + "Vince Staples" + "Joey Bada$$" + "Allan Kingdom" + "Noname Gypsy"
+
+  end
+
+  end
+
+puts selection ("Alternative")
